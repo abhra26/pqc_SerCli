8501441eea76d4780f74402a4707962d46386bf7
Abhraneel Saha <63891214+abhra26@users.noreply.github.com>
2024-12-01 01:49:47 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,24 @@
 import socket
 import oqs
-from x509 import CustomX509Certificate, X509Extension
+from x509 import CustomX509Certificate
+import os
+import json
+
+
+signerUSR = oqs.Signature('Dilithium3')
+pubKeyUSR = signerUSR.generate_keypair()
+
+client_cert = CustomX509Certificate(
+    subject="CN=Client,O=My Organization,C=US",
+    issuer="CN=My CA,O=My Organization,C=US",
+    public_key=pubKeyUSR,
+    signer=signerUSR,
+    validity_days=365 
+)
+
+client_cert.add_extension("2.5.29.17", "DNS:www.myclient.com", critical=False)
+client_cert.sign()
+
 
 def start_client():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -8,30 +26,18 @@ def start_client():
     client_socket.connect(('localhost', 65432))
     
     # Receive server's certificate from server
-    server_cert_data = client_socket.recv(1024).decode()
+    server_cert_data = client_socket.recv(2048).decode().strip()
     
     print("Received Server Certificate:")
     print(server_cert_data)
 
-    # Generate client's certificate (for demonstration purposes)
-    
-    signerUSR = oqs.Signature('Dilithium3')
-    pubKeyUSR = signerUSR.generate_keypair()
-
-    client_cert = CustomX509Certificate(
-        subject="CN=Client,O=My Organization,C=US",
-        issuer="CN=My CA,O=My Organization,C=US",
-        public_key=pubKeyUSR,
-        signer=signerUSR,
-        validity_days=365 
-    )
-    
-    client_cert.add_extension("2.5.29.17", "DNS:www.myclient.com", critical=False)  # SAN example
-
-    signature = client_cert.sign()
-
     # Send client's certificate to the server
-    client_socket.sendall(client_cert.serialize().encode())
+    client_socket.sendall(client_cert.serialize_json().encode())
+
+    server_kem_cert = client_socket.recv(2048).decode().strip()
+
+    print('Server kem Cert:')
+    print(server_kem_cert)
 
     client_socket.close()
 
