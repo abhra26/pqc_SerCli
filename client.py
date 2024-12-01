cc543f4b8e6d9f18dea7d5594de5c16c8330e580
Abhraneel Saha <63891214+abhra26@users.noreply.github.com>
2024-12-01 19:38:00 +0530

    Update client.py

--- client.py
+++ client.py
@@ -3,6 +3,9 @@ import oqs
 from x509 import CustomX509Certificate
 import os
 import json
+import time
+import base64
+import hashlib
 
 
 signerUSR = oqs.Signature('Dilithium3')
@@ -19,6 +22,8 @@ client_cert = CustomX509Certificate(
 client_cert.add_extension("2.5.29.17", "DNS:www.myclient.com", critical=False)
 client_cert.sign()
 
+shared_secret = os.urandom(32)
+kemobj = oqs.KeyEncapsulation('Kyber768')
 
 def start_client():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -26,18 +31,40 @@ def start_client():
     client_socket.connect(('localhost', 65432))
     
     # Receive server's certificate from server
-    server_cert_data = client_socket.recv(2048).decode().strip()
+    server_cert_data = json.loads(client_socket.recv(65536).decode())
     
-    print("Received Server Certificate:")
-    print(server_cert_data)
+    print("Server Signature Public Key:")
+    print(base64.b64decode(server_cert_data['Public Key'].encode()))
 
     # Send client's certificate to the server
     client_socket.sendall(client_cert.serialize_json().encode())
+    
+    # buffer = ""
+    # while True:
+    #     parts = client_socket.recv(4096).decode()
+    #     print(len(parts))
+    #     if not parts:
+    #         break
+    #     buffer += parts
 
-    server_kem_cert = client_socket.recv(2048).decode().strip()
+    server_kem_cert = json.loads(client_socket.recv(65536).decode())
 
-    print('Server kem Cert:')
-    print(server_kem_cert)
+    server_kemPubKey = base64.b64decode(server_kem_cert['Public Key'].encode())
+    print('Server Encap Public Key:')
+    print(server_kemPubKey)
+
+    ciphertext, plaintext = kemobj.encap_secret(server_kemPubKey)
+    print("Shared Secret:")
+    print(base64.b64encode(plaintext).decode())
+    hashdata = hashlib.sha256(ciphertext).hexdigest()
+    signature = signerUSR.sign(hashdata.encode())
+
+    data = {
+         'Shared Secret Cipher' : base64.b64encode(ciphertext).decode(),
+         'Signature' : base64.b64encode(signature).decode()
+    }
+
+    client_socket.sendall(json.dumps(data).encode())
 
     client_socket.close()
 
