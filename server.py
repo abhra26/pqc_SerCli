4abafc3c51317a24de52f47d052e795faecf0186
Abhraneel Saha <63891214+abhra26@users.noreply.github.com>
2024-12-02 22:11:48 +0530

    Update server.py

--- server.py
+++ server.py
@@ -6,9 +6,13 @@ import time
 import threading
 import hashlib
 import base64
+import aes
+
+LOCALHOST = 'localhost'
+HOST = '192.168.137.197'
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('localhost', 65432))
+server_socket.bind((HOST, 65432))
 server_socket.listen(1)
 
 signer = oqs.Signature('Dilithium3')
@@ -24,10 +28,30 @@ server_cert = CustomX509Certificate(
 
 server_cert.add_extension("2.5.29.17", "DNS:www.myserver.com", critical=False)  # SAN example
 
+
 server_cert.sign()
 
 print("Server is listening for connections...")
 
+# def pad_json(json_string):
+#     """Pad the JSON string with null characters to make its length a multiple of 256."""
+#     length = len(json_string)
+#     padding_needed = (256 - (length % 256)) % 256
+#     return json_string + '\0' * padding_needed
+
+# def sendall(json_string,conn):
+#     json_string = pad_json(json_string)
+#     for i in range(0,len(json_string),256):
+#         packet = json_string[i:i+256].encode()
+#         conn.sendall(packet)
+
+# def readall(conn):
+#     buffer = ""
+#     while True:
+#         packet = conn.recv(2048).decode()
+#         if '\0' in packet:
+#             break
+#         buffer
 
 def start_server():
     global server_socket
@@ -41,11 +65,19 @@ def start_server():
         # Sign and serialize the certificate
         
         # Send server's certificate to client
+        time.sleep(0.5)
         conn.sendall(server_cert.serialize_json().encode())
-        
+        conn.sendall('\0'.encode())
         # Receive client's certificate
 
-        client_cert_data = json.loads(conn.recv(65536).decode())
+        buffer = ""
+        while True:
+            parts  = conn.recv(2048).decode()
+            # print(len(parts))
+            if parts == '\0':
+                break
+            buffer += parts
+        client_cert_data = json.loads(buffer)
 
         client_pubKey_sign = base64.b64decode(client_cert_data['Public Key'].encode())
 
@@ -64,8 +96,16 @@ def start_server():
 
         # time.sleep(0.5)
         conn.sendall(kem_cert.serialize_json().encode())
+        conn.sendall('\0'.encode())
 
-        shared_secret_data = json.loads(conn.recv(65536).decode())
+        buffer = ""
+        while True:
+            parts = conn.recv(65536).decode()
+            if parts == '\0':
+                break
+            buffer += parts
+
+        shared_secret_data = json.loads(buffer)
         signature = base64.b64decode(shared_secret_data['Signature'].encode())
         cipher = base64.b64decode(shared_secret_data['Shared Secret Cipher'].encode())
         hash_data = hashlib.sha256(cipher).hexdigest()
@@ -73,6 +113,13 @@ def start_server():
         print(' ')
         print("Verification/AUTH:", signer.verify(hash_data.encode(),signature,client_pubKey_sign))
         print('shared secret:', base64.b64encode(kem.decap_secret(cipher)).decode())
+
+        shared_secret_data = kem.decap_secret(cipher)
+
+        cipher_data = aes.encrypt_txt('hello.txt',shared_secret_data,signer)
+
+        conn.sendall(cipher_data.encode())
+        conn.sendall('\0'.encode())
 
         if time.time() - start_time > 600:
             break
