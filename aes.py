07938468b8a15ad23ba91dd97c56a0a15ad101f7
Abhraneel Saha <63891214+abhra26@users.noreply.github.com>
2024-12-03 15:49:06 +0530

    Update aes.py

--- aes.py
+++ aes.py
@@ -36,6 +36,50 @@ def encrypt_image(image_path, key, signer):
     
     return json.dumps(data)
 
+def encrypt(plaintext,key,signer):
+    iv = os.urandom(12)
+
+    cipher = Cipher(algorithms.AES(key),modes.GCM(iv),backend=default_backend())
+    encryptor = cipher.encryptor()
+
+    ciphertext = encryptor.update(plaintext.encode()) + encryptor.finalize()
+
+    tag = encryptor.tag
+
+    hash = hashlib.sha256(ciphertext).hexdigest()
+    signature = signer.sign(hash.encode())
+
+    data = {
+        'iv' : base64.b64encode(iv).decode(),
+        'tag' : base64.b64encode(tag).decode(),
+        'Ciphertext' : base64.b64encode(ciphertext).decode(),
+        'Signature' : base64.b64encode(signature).decode()
+    }
+
+    return json.dumps(data)
+
+def decrypt(encrypted_data, key, signer,pubKey):
+    # Load the encrypted data from JSON
+    data = json.loads(encrypted_data)
+    iv = base64.b64decode(data['iv'].encode())
+    tag = base64.b64decode(data['tag'].encode())
+    ciphertext = base64.b64decode(data['Ciphertext'].encode())
+    signature = base64.b64decode(data['Signature'].encode())
+
+
+    cipher = Cipher(algorithms.AES(key), modes.GCM(iv), backend=default_backend())
+    decryptor = cipher.decryptor()
+
+    hash = hashlib.sha256(ciphertext).hexdigest()
+    if not signer.verify(hash.encode(),signature,pubKey):
+        raise ValueError("Invalid signature!")
+
+    # Decrypt the ciphertext
+    plaintext = decryptor.update(ciphertext) + decryptor.finalize()
+
+
+    return plaintext.decode()
+
 def encrypt_txt(file_path, key, signer):
     iv = os.urandom(12)
 
